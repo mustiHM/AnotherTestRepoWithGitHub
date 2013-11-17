457097311e58852810260344a4e4764759ecb068
mustiHM <qasmi@hm.edu>
2013-11-17 15:33:34 +0100

    Testklasse erstellt. Bitte hier Namen eintragen.

---
+++ MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/logic/Test.java
@@ -0,0 +1,9 @@
+package com.healthcaresolutions.norgine.moviprepcolonoprep.logic;
+
+public class Test {
+
+	// bitte fügt zum Testen einfach euren Namen mit in den String loggedInUsers rein.
+	// anschließend speichern, commiten und pushen
+	public String loggedInUsers = "Musti";
+	
+}
