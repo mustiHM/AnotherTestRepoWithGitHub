3313c3c68386d8c0f215dea5549cbde285ae8abe
mustiHM <qasmi@hm.edu>
2013-11-17 21:45:18 +0100

    Erweiterung der Logikschicht

--- MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/common/Appointment.java
+++ MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/common/Appointment.java
@@ -13,4 +13,41 @@ public class Appointment {
 	private int hour;
 	private int min;
 	
+	public String getHospital() {
+		return hospital;
+	}
+	public void setHospital(String hospital) {
+		this.hospital = hospital;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMin() {
+		return min;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
+	
 }
--- MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/logiclayer/AppointmentAdministration.java
+++ MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/logiclayer/AppointmentAdministration.java
@@ -1,7 +1,28 @@
 package com.healthcaresolutions.norgine.moviprepcolonoprep.logiclayer;
 
+import com.healthcaresolutions.norgine.moviprepcolonoprep.common.Appointment;
+
+/**
+ * Bietet Services zum Verwalten des Untersuchungstermins an
+ */
 public interface AppointmentAdministration {
 	
-	
+	/**
+	 * Speichert einen Untersuchungstermin in der Datenbank ab.
+	 * @param a der Untersuchungstermin
+	 */
+	public void addAppointment(Appointment a);
 
+	/**
+	 * Aktualisiert die Daten eines Untersuchungstermins.
+	 * @param a der neue Untersuchungstermin
+	 * @throws Exception falls vorher keine Daten gespeichert waren
+	 */
+	public void updateAppointment(Appointment a) throws Exception;
+	
+	/**
+	 * Löscht den gespeicherten Untersuchungstermin
+	 * @throws Exception falls kein Datensatz zum Löschen existiert.
+	 */
+	public void deleteAppointment() throws Exception;
 }
