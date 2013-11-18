04c6b6c2103fd2e62c85d30918cf4559786195eb
mustiHM <qasmi@hm.edu>
2013-11-18 22:14:15 +0100

    Fertige Implementierung des MedicinceService

---
+++ MoviPrepColonoprep/src/com/healthcaresolutions/norgine/moviprepcolonoprep/logiclayer/impl/MedicineServiceImpl.java
@@ -0,0 +1,31 @@
+package com.healthcaresolutions.norgine.moviprepcolonoprep.logiclayer.impl;
+
+import com.healthcaresolutions.norgine.moviprepcolonoprep.common.Medicine;
+import com.healthcaresolutions.norgine.moviprepcolonoprep.datalayer.ConfigReader;
+import com.healthcaresolutions.norgine.moviprepcolonoprep.datalayer.impl.ConfigReaderImpl;
+import com.healthcaresolutions.norgine.moviprepcolonoprep.logiclayer.MedicineService;
+
+public class MedicineServiceImpl implements MedicineService {
+
+	private Medicine defaultMedicine; // Standart-Medikament, falls keine Config verfügbar.
+	private ConfigReader cr = new ConfigReaderImpl();
+	
+	public Medicine getMedicineInfo() {
+		
+		defaultMedicine = cr.getMedicine();
+		
+		if(defaultMedicine == null){
+			initialize();
+		}
+		
+		return defaultMedicine;
+	}
+	
+	private void initialize(){
+		defaultMedicine = new Medicine();
+		defaultMedicine.setName("Test-Medikament");
+		defaultMedicine.setDescribtion("Das ist ein Test-Medikament das genutzt wird, weil keine Config verfügbar ist.");
+		defaultMedicine.setLink("http://www.google.de");
+	}
+
+}
