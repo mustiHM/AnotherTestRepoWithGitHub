c1f5e394e18e5e329698a2a898d2fa728797b227
nastiHM <sarwary@hm.edu>
2013-11-20 16:13:30 +0100

    Anlegen der ersten GUI

--- MoviPrepColonoprep/gen/com/healthcaresolutions/norgine/moviprepcolonoprep/R.java
+++ MoviPrepColonoprep/gen/com/healthcaresolutions/norgine/moviprepcolonoprep/R.java
@@ -22,9 +22,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int moviprep_5=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnAdministrateAppointment=0x7f080001;
+        public static final int btnCheckList=0x7f080007;
+        public static final int btnFoodList=0x7f080005;
+        public static final int btnInfo=0x7f08000b;
+        public static final int btnPreparation=0x7f080003;
+        public static final int btnSettings=0x7f080009;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f080008;
+        public static final int button4=0x7f08000a;
+        public static final int button5=0x7f08000c;
+        public static final int imgLogo=0x7f08000d;
+        public static final int lblWelcome=0x7f080000;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +49,14 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int administrateAppointment=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int checklist=0x7f050006;
+        public static final int foodList=0x7f050005;
+        public static final int info=0x7f050008;
+        public static final int preparation=0x7f050004;
+        public static final int settings=0x7f050007;
+        public static final int welcome=0x7f050002;
     }
     public static final class style {
         /** 
